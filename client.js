653ace7256e5e0dea1d04e2cb38e0e17fbaf1b9a
Sam Bahta <smabahta37@gmail.com>
2018-07-30 14:30:37 -0500

    sort of close

---
+++ client.js
@@ -0,0 +1,30 @@
+console.log('JS');
+$(readyNow);
+function readyNow() {
+    console.log('JQ');
+    $('#generatebtn').on('click', counterFunc)
+    $('#generatebtn').on('click', clickFunc)
+    $('#swapBtn').on('click',swapFunc)
+}
+
+let clicks = 0
+
+function counterFunc() {
+    $('#generatebtn').append('<div id="buttonDiv"><p id=counter></p></div>')   
+    if (clicks ===1 ){
+        $('.container').append('<button id="swapBtn">swap</button>');
+        $('.container').append('<button id="deletebtn">Delete</button>')
+   
+    }
+}
+
+function clickFunc() {
+    $('#counter').text(++clicks);
+   $('#counter').text(Number(clicks));
+    console.log(clicks);
+}
+
+$('#swapBtn').on('click',swapFunc)
+function swapFunc() {
+   console.log("swappy");
+}//still need to connect swap button and delete button 
